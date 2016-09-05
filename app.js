89af18c7e24cce4ec085fd6c6879612d20901994
dratech <alfonsotech@nym.hush.com>
2016-09-05 14:29:59 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var search_term;
+
+
+/*SET SEARCH REQUEST BY TYPE*/
+//Grab vale of serach query when button is clicked and store in "search_term" var
+$(function(){
+  $('.philosopher').click(function(event){
+    event.preventDefault();
+    search_term = $(this).attr('data-value');
+    console.log(search_term);
+    showResults(search_term);
+  });
+
+  $('.movement').click(function(event){
+    event.preventDefault();
+    search_term = $(this).attr('data-value');
+    console.log(search_term);
+    showResults(search_term);
+  });
+
+  $('.tropes').click(function(event){
+    event.preventDefault();
+    search_term = $(this).attr('data-value');
+    console.log(search_term);
+    showResults(search_term);
+  });
+}); //doc ready end
+
+//Render search query to search-results div by changing the src attribute of the <iframe>
+function showResults (search_term) {
+  //Build search query string
+  var url = 'http://plato.stanford.edu/entries/' + search_term + '/';
+  console.log(url);
+  $('.search-results').attr('src', url);
+    /*make unwanted elements disappear $('#seach-form').html('');*/
+    /*HT add reading times to articles*/
+}
+
+//Change search query string for fallback
