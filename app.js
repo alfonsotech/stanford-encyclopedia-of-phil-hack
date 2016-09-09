af43774297d09e91b3c107941e5893b3e2fe64f9
dratech <alfonsotech@nym.hush.com>
2016-09-09 03:53:04 -0700

    fix

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
 'use strict';
+$(document).ready(function() {
 
-var search_term;
+  var search_term;
+  /*SET SEARCH REQUEST BY TYPE*/
+  //Grab vale of serach query when button is clicked and store in "search_term" var
+  $(function(){
+    $('.philosopher').click(function(event){
+      event.preventDefault();
+      search_term = $(this).attr('data-value');
+      showResults();
+    });
 
+    $('.movement').click(function(event){
+      event.preventDefault();
+      search_term = $(this).attr('data-value');
+      showResults();
+    });
 
-/*SET SEARCH REQUEST BY TYPE*/
-//Grab vale of serach query when button is clicked and store in "search_term" var
-$(function(){
-  $('.philosopher').click(function(event){
-    event.preventDefault();
-    search_term = $(this).attr('data-value');
-    showResults(search_term);
-  });
+    $('.tropes').click(function(event){
+      event.preventDefault();
+      search_term = $(this).attr('data-value');
+      showResults();
+    });
+  }); //doc ready end
 
-  $('.movement').click(function(event){
-    event.preventDefault();
-    search_term = $(this).attr('data-value');
-    showResults(search_term);
-  });
-
-  $('.tropes').click(function(event){
-    event.preventDefault();
-    search_term = $(this).attr('data-value');
-    showResults(search_term);
-  });
-}); //doc ready end
-
-//Render search query to search-results div by changing the src attribute of the <iframe>
-function showResults (search_term) {
-  //Build search query string
-  var url = 'https://crossorigin.me/https://plato.stanford.edu/entries/adorno/' + search_term + '/';
-  $('.search-results').attr('src', url);
-    /*make unwanted elements disappear $('#seach-form').html('');*/
-    /*HT add reading times to articles*/
-}
-
-//Change search query string for fallback
+  //Render search query to search-results div by changing the src attribute of the <iframe>
+  function showResults () {
+    //Build search query string
+    var url = 'https://plato.stanford.edu/entries/' + search_term + '/';
+    console.log(url);
+    $('#search-results').attr('src', url);
+      /*make unwanted elements disappear $('#seach-form').html('');*/
+      /*HT add reading times to articles*/
+  }
+//https://crossorigin.me/
+});
